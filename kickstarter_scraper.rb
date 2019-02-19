0dec03e5b320977cb9a02cf09c6003606428aa90
Derek Cerretani <derekcerretani@gmail.com>
2019-02-19 22:19:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ require 'pry'
 html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
 
+# projects: kickstarter.css("li.project.grid_4")
+
 
 def create_project_hash
   # write your code here
