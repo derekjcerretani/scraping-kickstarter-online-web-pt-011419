7baf16463d21e85f330493b62c286f71b07321dd
Derek Cerretani <derekcerretani@gmail.com>
2019-02-19 22:32:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ kickstarter = Nokogiri::HTML(html)
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
-# percent funded: project.css("ul.project-stats li.first.funded strong").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
   # write your code here
